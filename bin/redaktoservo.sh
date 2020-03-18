2243596ececa583d1e8ac1c16686d3d99d6ffca0
revo <revo@retavortaro.de>
2020-03-18 20:21:09 +0100

    kor. vokon de resumo kaj nokta kuro

--- bin/redaktoservo.sh
+++ bin/redaktoservo.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 datetime=$(date +%Y%m%d_%H%M%S)
+target="-Duser-mail-file-exists=true srv-servo"
 
 cd ${REVO}
 
 while getopts "rap" OPT; do
   case ${OPT} in
     r)
-      command="ant -file ${VOKO}/ant/redaktoservo.xml srv-resumo 2>&1"
+      target="srv-resumo"
+      # ne skribu protokolon, nur sendu tra cron!
+      command="ant -file ${VOKO}/ant/redaktoservo.xml srv-resumo"
       echo -e "${command}\nTIME:" $(date)"\n"
-      exec ${command}
+      exec ${command} 2>&1
       ;;
     a)
       target="srv-servo-art"
@@ -18,14 +21,12 @@ while getopts "rap" OPT; do
       target="srv-purigu"
       ;;
     *)
-      # nepre rekreu la vortaron eĉ se tiufoje ne alvenis poŝto  
-      target="-Duser-mail-file-exists=true srv-servo"
-      # target="srv-servo"
+      # ĉu eldoni informojn kiel uzi...?
   esac
 done
 
 log="${HOME}/revolog/redsrv-${datetime}.log"
-command="ant -file ${VOKO}/ant/redaktoservo.xml ${target} 2>&1 | tee ${log}"
-echo -e "${command}\nTIME:" $(date)"\n"
-exec ${command}
+command="ant -file ${VOKO}/ant/redaktoservo.xml ${target}"
+echo -e "${command}\nTIME:" $(date)" > ${log}\n"
+exec ${command} 2>&1 | tee -a ${log}
 
