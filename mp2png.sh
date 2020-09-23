131846c5ab908c4a4d07e8ebf6e3c05e3d43dc84
Wolfram Diestel <wolfram.diestel@t-systems.com>
2020-09-23 17:33:51 +0200

    kreado de png  per mpost anst. svg-convert...

--- mp2png.sh
+++ mp2png.sh
@@ -3,18 +3,20 @@ set +e
 set +x
 
 MP=mpost
-RSVG=rsvg-convert
+# tiu ne bone funkcias por eltondi formon el alia kiel ĉe GRA, FON
+# anstataŭe mpost kreas ambau svg kaj png el mp-dosiero!
+# RSVG=rsvg-convert
 
 cd smb
 
 for file in ???.mp ????.mp
 do
-    svg=${file%%.mp}.svg
-    echo ${MP} ${file} ${svg}
-    ${MP} ${file} ${svg}
-    png=${file%%.mp}.png
-    echo ${RSVG} -w 100 -h 100 -o ${png} ${svg}
-    ${RSVG} -w 100 -h 100 -o ${png} ${svg}
+    #svg=${file%%.mp}.svg
+    echo ${MP} ${file} #${svg}
+    ${MP} ${file} #${svg}
+    #png=${file%%.mp}.png
+    #echo ${RSVG} -w 100 -h 100 -o ${png} ${svg}
+    #${RSVG} -w 100 -h 100 -o ${png} ${svg}
 done
 
 cd ..
