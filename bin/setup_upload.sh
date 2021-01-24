04d40fd6d1207da7204c8b9b3887083262400157
Wolfram Diestel <diestel@steloj.de>
2021-01-24 16:01:01 +0100

    + FTP_*2 kaj cGI_*2 por nova servilo

--- bin/setup_upload.sh
+++ bin/setup_upload.sh
@@ -33,15 +33,42 @@ fi
 #CGI_USER=araneo
 
 if [[ -z "$CGI_SERVER" ]]; then
-  CGI_SERVER=$(cat ${SECRETS}/voko-sesio.cgi_server)
+  CGI_SERVER=$(cat ${SECRETS}/voko-araneo.cgi_server)
 fi
 
 if [[ -z "$CGI_USER" ]]; then
-  CGI_USER=$(cat ${SECRETS}/voko-sesio.cgi_user)
+  CGI_USER=$(cat ${SECRETS}/voko-araneo.cgi_user)
 fi
 
 if [[ -z "$CGI_PASSWD" ]]; then
-  CGI_PASSWD=$(cat ${SECRETS}/voko-sesio.cgi_password)
+  CGI_PASSWD=$(cat ${SECRETS}/voko-araneo.cgi_password)
+fi
+
+### por la transiro al nova servilo ni subtenas
+# duan agordon...
+
+if [[ -z "$FTP_SERVER2" ]]; then
+  FTP_SERVER2=$(cat ${SECRETS}/voko-sesio.ftp_server2)
+fi
+
+if [[ -z "$FTP_USER2" ]]; then
+  FTP_USER2=$(cat ${SECRETS}/voko-sesio.ftp_user2)
+fi
+
+if [[ -z "$FTP_PASSWD2" ]]; then
+  FTP_PASSWD2=$(cat ${SECRETS}/voko-sesio.ftp_password2)
+fi
+
+if [[ -z "$CGI_SERVER2" ]]; then
+  CGI_SERVER2=$(cat ${SECRETS}/voko-araneo.cgi_server2)
+fi
+
+if [[ -z "$CGI_USER2" ]]; then
+  CGI_USER2=$(cat ${SECRETS}/voko-araneo.cgi_user2)
+fi
+
+if [[ -z "$CGI_PASSWD2" ]]; then
+  CGI_PASSWD2=$(cat ${SECRETS}/voko-araneo.cgi_password2)
 fi
 
 # debug
@@ -58,14 +85,28 @@ servilo.host=${FTP_SERVER}
 servilo.user=${FTP_USER}
 servilo.password=${FTP_PASSWD}
 
+servilo2.dir=${FTP_DIR}
+servilo2.host=${FTP_SERVER2}
+servilo2.user=${FTP_USER2}
+servilo2.password=${FTP_PASSWD2}
+
 tempo.url=http://${CGI_SERVER}/cgi-bin/admin/time.pl
 upload.url=http://${CGI_SERVER}/cgi-bin/admin/uprevo.pl?fname=
 upload.user=${CGI_USER}
 upload.password=${CGI_PASSWD}
 
+upload2.url=http://${CGI_SERVER2}/cgi-bin/admin/uprevo.pl?fname=
+upload2.user=${CGI_USER2}
+upload2.password=${CGI_PASSWD2}
+
 vikio.url=http://${CGI_SERVER}/cgi-bin/admin/upviki.pl?download=0
 vikio.user=${CGI_USER}
 vikio.password=${CGI_PASSWD}
+
+vikio2.url=http://${CGI_SERVER2}/cgi-bin/admin/upviki.pl?download=0
+vikio2.user=${CGI_USER2}
+vikio2.password=${CGI_PASSWD2}
+
 EOT
 
 else
