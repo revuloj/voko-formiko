fa3b794a978f7c45de1d45967a17c42553a8a44c
Wolfram Diestel <diestel@steloj.de>
2025-08-08 13:07:16 +0200

    +komento

--- bin/setup_crontab.sh
+++ bin/setup_crontab.sh
@@ -6,6 +6,8 @@ echo "### Prepari tempotaskojn (cron)..."
 # debug
 ls -l /config/*
 
+# kreata per `docker config create ...` ĉe docker swarm 
+# vd. revo-medioj/formikujo-s/bin/voko-agordo-cron-*
 cronfile=/config/voko-formiko.agordo-cron
 
 #chmod 0644 ${cronfile}
--- bin/setup_redaktoservo.sh
+++ bin/setup_redaktoservo.sh
@@ -8,6 +8,8 @@ export CLASSPATH=$SAXONJAR:$JINGJAR
 # debug
 ls -l /config/*
 
+# kreata per `docker config create ...` ĉe docker swarm 
+# vd. revo-medioj/formikujo-s/bin/voko-agordo-redaktoservo-*
 cfgfile=/config/voko-formiko.agordo-redaktoservo
 
 ln -s ${cfgfile} /home/formiko/etc/redaktoservo-agordo
